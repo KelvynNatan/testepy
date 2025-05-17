ce84aad5ac1e94c449e9cdf7b7a8b341c1887b3a
KelvynNatan <Kelvynnatan456@gmail.com>
2025-05-17 18:54:30 -0300

    POO

--- ex1.py
+++ ex1.py
@@ -1,1 +1,12 @@
-print ("oi")
+class carro:
+    def __init__(self, marca, modelo, ano, motor):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.motor = motor
+    
+uno = carro("Fiat", "Uno", 2010, 1.0)
+gol = carro("Volkswagem", "Gol", 2012, 2.0)
+
+print(uno.motor)
+print(gol.motor)
